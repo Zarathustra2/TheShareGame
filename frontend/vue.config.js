d2866181baaa55a942de363ed1b4f9415d22589f
Zarathustra2 <dario.heinisch@gmail.com>
2020-05-08 12:43:34 +0200

    Frontend: Fixing CircleCI Memory issues
    We still encounter memory issues on CircleCI when building the
    frontend. As stated in the docs it is not enough to disable
    the parallel build, we have to set the explicit number of CPUs
    to use.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const CompressionPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const os = require('os');
 
 
 const isCirleCi = () => {
@@ -31,9 +32,15 @@ module.exports = {
           //
           // So we disable the parallel build, if we run on CircleCI.
           //
+          // Disabling the parallel build is apparently not enough so we have to
+          // set the amount of workers explicitly:
+          //       If you use Circle CI or any other environment that doesn't provide real available
+          //       count of CPUs then you need to setup explicitly number of
+          //       CPUs to avoid Error: Call retries were exceeded
+          //
           // See: https://github.com/webpack-contrib/terser-webpack-plugin/issues/143
           // See: https://github.com/webpack-contrib/terser-webpack-plugin/issues/202
-          parallel: !isCirleCi(),
+          parallel: (!isCirleCi()) ? os.cpus().length - 1 : 1,
         }),
       ],
     },
