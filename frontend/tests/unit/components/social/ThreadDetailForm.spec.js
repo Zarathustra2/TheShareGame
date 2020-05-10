29466d28bf69c6f4f7e203827df9c78e6516bfc6
Zarathustra2 <dario.heinisch@gmail.com>
2020-05-10 23:16:02 +0200

    Frontend: Tests for ThreadDetail- & ThreadForm

--- frontend/tests/unit/components/social/ThreadDetailForm.spec.js
+++ frontend/tests/unit/components/social/ThreadDetailForm.spec.js
@@ -1,44 +1,60 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 
-import VueRouter from 'vue-router';
 import ThreadDetailForm from '@/components/social/ThreadDetailForm.vue';
 
 const localVue = createLocalVue();
-const router = new VueRouter();
-localVue.use(VueRouter);
 
+const validText = 'hey';
+const inValidText = 'invalid';
 
 describe('ThreadDetailForm', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = mount(ThreadDetailForm, {
       localVue,
-      router,
       mocks: {
+        $route: {
+          params: { id: 1 },
+        },
         $http: {
-
+          post(_, data) {
+            if (data.text === validText) {
+              return Promise.resolve();
+            }
+            return Promise.reject({
+              response: {
+                data: {
+                  text: 'Text too long!',
+                },
+              },
+            });
+          },
         },
       },
     });
   });
 
   it('displays error messages', async () => {
-    const keys = ['textErrMsg', 'errMessage'];
+    wrapper.setData({ form: { text: inValidText } });
+    wrapper.find('.submit-btn').trigger('submit');
 
-    const data = {
-      formFeedback: {
-        textValid: false,
-        textErrMsg: 'Name Error!',
-        isValid: false,
-        errMessage: 'Err Message!',
-      },
-    };
-
-    wrapper.setData(data);
+    await wrapper.vm.$nextTick();
     await wrapper.vm.$nextTick();
 
-    keys.forEach((e) => {
-      expect(wrapper.html()).toContain(data.formFeedback[e]);
+    expect(wrapper.html()).toContain('Text too long!');
+  });
+
+  it('emits event if new thread post has been created', async () => {
+    wrapper.setData({
+      form: {
+        text: validText,
+      },
     });
+
+    wrapper.find('.submit-btn').trigger('submit');
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted('forceReload')).toBeTruthy();
   });
 });
